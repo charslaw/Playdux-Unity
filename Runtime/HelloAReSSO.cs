f4a95fcbdc3e94ea14964c47ed8804591b22571d
Cole Schultz <schultzcole@gmail.com>
2020-02-20 21:55:53 -0600

    Add package.json and some experimentation scripts

---
+++ Runtime/HelloAReSSO.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace AReSSO.Runtime
+{
+    public class HelloAReSSO : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("Hello from AReSSO!");
+        }
+    }
+}
---
+++ Tests/Editor/HelloAReSSOTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace AReSSO.Tests.Editor
+{
+    public class HelloAReSSOTests
+    {
+        [Test]
+        public void BasicTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
