312be7bcacd6955c9aa963f8e7939cb856ab3ce2
Cole Schultz <schultzcole@gmail.com>
2020-03-03 19:28:43 -0600

    ISSUE #11 fix doc comments on InitializeAction

--- src/Store/InitializeAction.cs
+++ src/Store/InitializeAction.cs
@@ -1,12 +1,8 @@
 namespace AReSSO.Store
 {
     /// <summary>
-    /// The InitializeAction is a special reserved action which must be used to initialize a Store.
+    /// The InitializeAction is a special reserved action which can be used to initialize a Store.
     /// </summary>
-    /// <remarks>
-    /// If a store is not initialized with this action prior to being used,
-    /// it will throw a StoreNotInitializedException.
-    /// </remarks>
     public class InitializeAction<TRootState> : IAction
     {
         public TRootState InitialState { get; }
