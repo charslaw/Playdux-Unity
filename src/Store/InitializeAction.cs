a9bf4ee9445b696ae06b7054ebb55f6015d354cc
Cole Schultz <schultzcole@gmail.com>
2021-04-12 16:33:05 -0500

    Remove unused `InitializeActionStateType` method

--- src/Store/InitializeAction.cs
+++ src/Store/InitializeAction.cs
@@ -6,18 +6,4 @@ namespace AReSSO.Store
     /// The InitializeAction is a special reserved action which can be used to initialize a Store.
     /// </summary>
     public record InitializeAction<TRootState>(TRootState InitialState) : IAction;
-
-    internal static class InitializeHelper
-    {
-        /// Gets the type of state object in an InitializeAction
-        /// Returns null if the action is not an InitializeAction
-        internal static Type InitializeActionStateType(IAction action)
-        {
-            var actionType = action.GetType();
-            var actionIsInitializeAction = actionType.IsGenericType &&
-                actionType.GetGenericTypeDefinition() == typeof(InitializeAction<>);
-
-            return actionIsInitializeAction ? actionType.GetGenericArguments()[0] : null;
-        }
-    }
 }
